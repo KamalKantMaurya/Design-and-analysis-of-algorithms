30edaceff74698e9f537e92d210146ebdd1bf8c3
Kamal Kant Maurya <mauryakamalkant424@gmail.com>
2023-05-28 18:43:40 +0530

    linear  search

---
+++ Week1/Problem1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int key,n,t,arr[100],count;
+    printf("Enter the number of test cases:\n");
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        count=0;
+        printf("\nEnter the size of the array:\n");
+        scanf("%d",&n);
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&arr[j]);
+        }
+        printf("Enter the element you want to search:\n");
+        scanf("%d",&key);
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]==key)
+            {
+                printf("%d present",key);
+                count=1;
+            }
+        }
+        if(count==0)
+        {
+            printf("%d not present",key);
+        }
+    }
+}
